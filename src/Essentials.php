9c054c6d47074ee4f4662a620204db5ab3042bac
fridde <f@hehl.se>
2016-11-22 21:12:34 +0100

    added bardump

--- src/Essentials.php
+++ src/Essentials.php
@@ -52,6 +52,11 @@ class Essentials
         Debugger::$logSeverity = E_NOTICE | E_WARNING;
     }
 
+    public static function barDump()
+    {
+        Debugger::barDump(func_get_args());
+    }
+
     public static function prePrint($var)
     {
         echo '<pre>';
