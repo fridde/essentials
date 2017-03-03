166906ac6895547efd05dd752b88e1fa30e45b91
fridde <f@hehl.se>
2017-03-03 09:06:06 +0100

    overhaul

--- src/Essentials.php
+++ src/Essentials.php
@@ -1,20 +1,18 @@
 <?php
 namespace Fridde;
 
-use \Yosymfony\Toml\Toml;
-use \Tracy\Debugger;
+use Yosymfony\Toml\Toml;
+use Tracy\Debugger;
+use Monolog\{ErrorHandler, Logger};
+use Monolog\Handler\StreamHandler;
 
-/**
-* This class contains the functions needed to start some applications and is
-* TODO:
-*/
 
 class Essentials
 {
 
     public static function setAppUrl($sub_dir = null)
     {
-        $base_dir = $GLOBALS["BASE_DIR"] ?? null;
+        $base_dir = defined('BASE_DIR') ? BASE_DIR : null;
         if(!empty($base_dir) && empty($sub_dir)){
             $sub_dir = basename($base_dir);
         }
@@ -22,26 +20,32 @@ class Essentials
         if(substr($sub_dir, -1 , 1) !== "/"){
             $sub_dir .= "/";
         }
-        $GLOBALS["APP_URL"] = $_SERVER['HTTP_HOST'] . "/" . $sub_dir;
+        if(!defined('APP_URL')){
+            define('APP_URL', $_SERVER['HTTP_HOST'] . "/" . $sub_dir);
+        }
+        $GLOBALS["APP_URL"] = APP_URL; // for backwards-compatibility
     }
 
     /**
-     * [setBaseUrl description]
-     * @param [type] $dir     Should always be __DIR__, called from the index file
-     * @param string $app_dir [description]
-     */
+    * [setBaseUrl description]
+    * @param [type] $dir     Should always be __DIR__, called from the index file
+    * @param string $app_dir [description]
+    */
     public static function setBaseDir($dir)
     {
         if(substr($dir, -1 , 1) !== "/"){
             $dir .= '\\';
         }
-        $GLOBALS["BASE_DIR"] = $dir;
+        if(!defined('BASE_DIR')){
+            define('BASE_DIR', $dir);
+        }
+        $GLOBALS["BASE_DIR"] = $dir; // for backwards-compatibility
     }
 
     public static function getSettings($file = 'config/settings.toml', $prefix = "", $globalize = true)
     {
-        $possible_locations[] = $GLOBALS["APP_URL"] ?? null;
-        $possible_locations[] = $GLOBALS["BASE_DIR"] ?? null;
+        $possible_locations[] = defined('APP_URL') ? APP_URL : null;
+        $possible_locations[] = defined('BASE_DIR') ? BASE_DIR : null;
         $possible_locations[] = $_SERVER['DOCUMENT_ROOT'];
         $possible_locations[] = getcwd();
 
@@ -71,8 +75,9 @@ class Essentials
             throw new \Exception('The function getSettings has no implementation for the file extension <'.$ext.'>');
         }
 
-        if($settings && $globalize){
-            $GLOBALS[$prefix . "SETTINGS"] = $settings;
+        if($settings && $globalize && !defined($prefix . 'SETTINGS')){
+            define($prefix . 'SETTINGS', $settings);
+            $GLOBALS[$prefix . "SETTINGS"] = $settings; // for backwards-compatibility
         }
 
         return $settings;
@@ -85,16 +90,29 @@ class Essentials
         return $routes;
     }
 
-    public static function activateDebug()
+    public static function activateDebug($options = [])
     {
         $GLOBALS['debug'] = true;
+
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
 
-        Debugger::enable();
-        Debugger::$strictMode = TRUE;
-        Debugger::$logSeverity = E_NOTICE | E_WARNING;
-        Debugger::$maxDepth = 8;
+        if(!in_array("no_tracy", $options)){
+            Debugger::enable();
+            Debugger::$strictMode = TRUE;
+            Debugger::$logSeverity = E_NOTICE | E_WARNING;
+            Debugger::$maxDepth = 8;
+        }
+    }
+
+    public static function activateLogger()
+    {
+        if(empty($GLOBALS["LOGGER"])){
+            $logger = new Logger('Error handler');
+            $logger->pushHandler(new StreamHandler(BASE_DIR.'log/errors.log', Logger::DEBUG));
+            ErrorHandler::register($logger);
+            $GLOBALS["LOGGER"] = $logger;
+        }
     }
 
     public static function prePrint($var)
