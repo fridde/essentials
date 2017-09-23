561813f2b443582b15fffc48accc5e980b4d9567
fridde <f@hehl.se>
2017-09-23 20:23:44 +0200

    Updated logger

--- src/Essentials.php
+++ src/Essentials.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Fridde;
 
 
@@ -17,41 +18,41 @@ class Essentials
 {
 
     /**
-    * Defines $_SERVER['HTTP_HOST'] plus an optional subdirectory as the global constant APP_URL
-    *
-    * @param string $sub_dir An optional subdirectory below HTTP_HOST
-    * @return void
-    */
+     * Defines $_SERVER['HTTP_HOST'] plus an optional subdirectory as the global constant APP_URL
+     *
+     * @param string $sub_dir An optional subdirectory below HTTP_HOST
+     * @return void
+     */
     public static function setAppUrl($sub_dir = null)
     {
         $base_dir = defined('BASE_DIR') ? BASE_DIR : null;
-        if(!empty($base_dir) && empty($sub_dir)){
+        if (!empty($base_dir) && empty($sub_dir)) {
             $sub_dir = basename($base_dir);
         }
         $sub_dir = $sub_dir ?? "";
-        if(substr($sub_dir, -1 , 1) !== "/"){
+        if (substr($sub_dir, -1, 1) !== "/") {
             $sub_dir .= "/";
         }
-        if(!defined('APP_URL')){
-            define('APP_URL', $_SERVER['HTTP_HOST'] . "/" . $sub_dir);
+        if (!defined('APP_URL')) {
+            define('APP_URL', $_SERVER['HTTP_HOST']."/".$sub_dir);
         }
         $GLOBALS["APP_URL"] = APP_URL; // for backwards-compatibility
     }
 
     /**
-    * Sets a given directory as the global constant BASE_DIR. Use this function in
-    * your index file and give it __DIR__ as argument.
-    *
-    * @param string $dir     The directory to set as BASE_DIR. Should always be __DIR__,
-    *                        called from the index file
-    * @return void
-    */
+     * Sets a given directory as the global constant BASE_DIR. Use this function in
+     * your index file and give it __DIR__ as argument.
+     *
+     * @param string $dir The directory to set as BASE_DIR. Should always be __DIR__,
+     *                        called from the index file
+     * @return void
+     */
     public static function setBaseDir($dir)
     {
         $dir = rtrim($dir, '/\\');
         $dir .= DIRECTORY_SEPARATOR;
 
-        if(!defined('BASE_DIR')){
+        if (!defined('BASE_DIR')) {
             define('BASE_DIR', $dir);
         }
         $GLOBALS["BASE_DIR"] = $dir; // for backwards-compatibility
@@ -61,9 +62,13 @@ class Essentials
     {
         $routes = Settings::getArrayFromFile($file);
         $routes = array_filter($routes["routes"]);
-        array_walk($routes, function(&$v, $i){
-            $v[] = $i;
-        });
+        array_walk(
+            $routes,
+            function (&$v, $i) {
+                $v[] = $i;
+            }
+        );
+
         return array_values($routes);
     }
 
@@ -75,60 +80,57 @@ class Essentials
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
 
-        if(!in_array("no_tracy", $options)){
+        if (!in_array("no_tracy", $options)) {
             Debugger::enable();
-            Debugger::$strictMode = TRUE;
+            Debugger::$strictMode = true;
             Debugger::$logSeverity = E_NOTICE | E_WARNING;
             Debugger::$maxDepth = 8;
         }
     }
 
-    public static function getLogger(string $logger_type = "Info")
+    public static function getLogger(string $logger_name = "Logger")
     {
         $container = $GLOBALS["CONTAINER"] ?? null;
-        $logger_name = $logger_type . "Logger";
 
-        if(!empty($container) && $container->has($logger_name)){
+        if (!empty($container) && $container->has($logger_name)) {
             return $container->get($logger_name);
         }
         $logger = new Logger($logger_name);
-        if($logger_type == "Info"){
-            $stream = new StreamHandler(BASE_DIR.'log/info.log', Logger::DEBUG);
-            $stream->setFormatter(new LineFormatter());
-            $logger->pushHandler($stream);
-        } elseif($logger_type == "Error"){
-            $stream = new StreamHandler(BASE_DIR.'log/errors.log', Logger::DEBUG);
-            $stream->setFormatter(new LineFormatter());
-            $logger->pushHandler($stream);
-            $logger->pushHandler(new ChromePHPHandler());
-            ErrorHandler::register($logger);
-        } else {
-            throw new \Exception("The logger type <" . $logger_type . "> is not defined.");
-        }
+        $file_name = 'log_' . date('Y-m-d') . '.log';
+        $stream = new StreamHandler(BASE_DIR.'log/' . $file_name, Logger::DEBUG);
+        $stream->setFormatter(new LineFormatter());
+        $logger->pushHandler($stream);
+        $logger->pushHandler(new ChromePHPHandler());
+        ErrorHandler::register($logger);
+
         return $logger;
     }
 
     public static function registerSharedServices(...$services)
     {
-        if(count($services) === 1 && array_filter($services[0], "is_array") == $services[0]){
+        if (count($services) === 1 && array_filter($services[0], "is_array") == $services[0]) {
             $services = $services[0];
         }
 
         $container = new Container();
-        foreach($services as $service){
+        foreach ($services as $service) {
             $count = count($service);
-            if($count === 2){
+            if ($count === 2) {
                 $container->share($service[0], $service[1]);
-            } elseif($count > 2){
+            } elseif ($count > 2) {
                 $const_args = array_slice($service, 2);
-                array_walk($const_args, function(&$a){
-                    $a = new RawArgument($a);
-                });
+                array_walk(
+                    $const_args,
+                    function (&$a) {
+                        $a = new RawArgument($a);
+                    }
+                );
                 $container->share($service[0], $service[1])
-                ->withArguments($const_args);
+                    ->withArguments($const_args);
             }
         }
         $GLOBALS["CONTAINER"] = $container;
+
         return $container;
     }
 
@@ -142,7 +144,7 @@ class Essentials
     public static function registerDBLogger($entity_manager, $logger)
     {
         $pdo = $entity_manager->getConnection()->getWrappedConnection();
-        $mySQLHandler = new MySQLHandler($pdo, "errors", [] , Logger::DEBUG);
+        $mySQLHandler = new MySQLHandler($pdo, "log", ['source'], Logger::DEBUG);
         $logger->pushHandler($mySQLHandler);
     }
 
